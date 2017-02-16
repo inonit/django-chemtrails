808b31aa835c45bcf2c3e823e69d49eb8b80a5b2
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-02-16 11:46:33 +0100

    Added some unit tests

--- tests/test_neoutils_core.py
+++ tests/test_neoutils_core.py
@@ -5,15 +5,23 @@ from django.test import TestCase
 from django.utils import six
 
 from neomodel import *
+from neomodel.match import NodeSet
 
 from chemtrails.neoutils import MetaNodeMeta, MetaNodeMixin
-from chemtrails.neoutils import get_meta_node_class_for_model, get_node_class_for_model, get_node_for_object
+from chemtrails.neoutils import (
+    get_meta_node_class_for_model, get_node_class_for_model,
+    get_node_for_object, get_nodeset_for_queryset
+)
 
 from tests.testapp.autofixtures import StoreFixture
 from tests.testapp.models import Book, Store
 
 
-class ModelNodeUtilsTestCase(TestCase):
+class NodeUtilsTestCase(TestCase):
+    """
+    Test various utility functions for dealing with
+    model and node instances.
+    """
 
     def test_get_relations_node_class_for_model(self):
         klass = get_meta_node_class_for_model(Book)
@@ -28,41 +36,61 @@ class ModelNodeUtilsTestCase(TestCase):
         store_node = get_node_for_object(store)
         self.assertIsInstance(store_node, get_node_class_for_model(Store))
 
-    def test_get_node_for_multiple_objects(self):
-        stores = StoreFixture(Store).create(count=3, commit=True)
-        for store in stores:
-            store_node = get_node_for_object(store).sync()
-            self.assertIsInstance(store_node, get_node_class_for_model(Store))
+    def test_get_nodeset_for_queryset(self):
+        created = StoreFixture(Store).create(count=3, commit=True)
+        queryset = Store.objects.filter(pk__in=map(lambda n: n.pk, created))
+        nodeset = get_nodeset_for_queryset(queryset)
+        self.assertIsInstance(nodeset, NodeSet)
+        for node in nodeset:
+            self.assertIsInstance(node, get_node_class_for_model(queryset.model))
 
 
-class ModelRelationsNodeTestCase(TestCase):
+class MetaNodeTestCase(TestCase):
 
     def test_create_node(self):
         @six.add_metaclass(MetaNodeMeta)
-        class RelationNode(MetaNodeMixin, StructuredNode):
+        class MetaNode(MetaNodeMixin, StructuredNode):
             class Meta:
                 model = Book
-                app_label = 'test'
 
-        self.assertIsInstance(RelationNode(), StructuredNode)
+        self.assertTrue(issubclass(MetaNode, StructuredNode))
+        self.assertIsInstance(MetaNode(), StructuredNode)
+
+    def test_create_node_declaring_model_in_class(self):
+        @six.add_metaclass(MetaNodeMeta)
+        class MetaNode(MetaNodeMixin, StructuredNode):
+            __metaclass_model__ = Book
+            class Meta:
+                model = None
+
+        self.assertEqual(MetaNode.Meta.model, Book)
+
+    def test_create_node_custom_app_label(self):
+        @six.add_metaclass(MetaNodeMeta)
+        class MetaNode(MetaNodeMixin, StructuredNode):
+            class Meta:
+                model = Book
+                app_label = 'custom_app_label'
+
+        self.assertEqual(MetaNode.Meta.app_label, 'custom_app_label')
 
     def test_create_node_fails_without_meta_model(self):
         try:
             @six.add_metaclass(MetaNodeMeta)
-            class RelationNode(MetaNodeMixin, StructuredNode):
+            class MetaNode(MetaNodeMixin, StructuredNode):
                 class Meta:
                     model = None
 
-            self.fail('Did not fail when defining a ModelRelationNode with missing Meta class model.')
+            self.fail('Did not fail when defining a MetaNode with missing Meta class model.')
         except ValueError as e:
-            self.assertEqual(str(e), '%s.Meta.model attribute cannot be None.' % 'RelationNode')
+            self.assertEqual(str(e), '%s.Meta.model attribute cannot be None.' % 'MetaNode')
 
     def test_create_node_fails_without_meta_class(self):
         try:
             @six.add_metaclass(MetaNodeMeta)
-            class RelationNode(MetaNodeMixin, StructuredNode):
+            class MetaNode(MetaNodeMixin, StructuredNode):
                 pass
 
-            self.fail('Did not fail when defining a ModelRelationNode without a Meta class.')
+            self.fail('Did not fail when defining a MetaNode without a Meta class.')
         except ImproperlyConfigured as e:
-            self.assertEqual(str(e), '%s must implement a Meta class.' % 'RelationNode')
+            self.assertEqual(str(e), '%s must implement a Meta class.' % 'MetaNode')
