4748d4c43059dc4b29f7a1ecf4daebe132aac298
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-02-21 22:53:32 +0100

    Added test cases for settings

---
+++ tests/test_settings.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase, override_settings
+from chemtrails import settings
+
+
+class ChemtrailsSettingsTestCase(TestCase):
+
+    def test_default_settings(self):
+        self.assertEqual(settings.ENABLED, True)
+        self.assertEqual(settings.NAMED_RELATIONSHIPS, True)
+        self.assertEqual(settings.CONNECT_META_NODES, False)
+        self.assertEqual(settings.IGNORE_MODELS, ['migrations.migration'])
+
+    @override_settings(CHEMTRAILS={
+        'ENABLED': False,
+        'NAMED_RELATIONSHIPS': False,
+        'IGNORE_MODELS': [
+            'auth.user'
+        ]
+    })
+    def test_override_settings(self):
+        from chemtrails.settings import chemtrails_settings as settings  # Need to re-import after override
+        self.assertEqual(settings.ENABLED, False)
+        self.assertEqual(settings.NAMED_RELATIONSHIPS, False)
+        self.assertEqual(settings.CONNECT_META_NODES, False)
+        self.assertEqual(settings.IGNORE_MODELS, ['auth.user'])
