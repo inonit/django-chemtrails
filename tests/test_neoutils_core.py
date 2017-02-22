29ad5c7832032bbb4f453839d97d52d1b5a28391
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-02-22 14:13:50 +0100

    Updated unit tests

--- tests/test_neoutils_core.py
+++ tests/test_neoutils_core.py
@@ -14,25 +14,30 @@ from chemtrails.neoutils import (
     get_node_class_for_model, get_node_for_object, get_nodeset_for_queryset
 )
 
-from tests.utils import ChemtrailsTestCase, flush_nodes
+from tests.utils import flush_nodes
 from tests.testapp.autofixtures import BookFixture, StoreFixture
 from tests.testapp.models import Book, Store
 
 USER_MODEL = get_user_model()
 
 
-class NodeUtilsTestCase(ChemtrailsTestCase):
+class NodeUtilsTestCase(TestCase):
     """
     Test various utility functions for dealing with
     model and node instances.
     """
-    def test_get_relations_node_class_for_model(self):
+    def test_get_meta_node_class_for_model(self):
         klass = get_meta_node_class_for_model(Book)
         self.assertTrue(issubclass(klass, StructuredNode))
 
     def test_get_node_class_for_model(self):
         klass = get_node_class_for_model(Book)
         self.assertTrue(issubclass(klass, StructuredNode))
+
+    @flush_nodes()
+    def test_get_meta_node_for_model(self):
+        book_meta = get_meta_node_for_model(Book)
+        self.assertIsInstance(book_meta, StructuredNode)
 
     @flush_nodes()
     def test_get_node_for_object(self):
@@ -50,7 +55,7 @@ class NodeUtilsTestCase(ChemtrailsTestCase):
             self.assertIsInstance(node, get_node_class_for_model(queryset.model))
 
 
-class ModelNodeTestCase(ChemtrailsTestCase):
+class ModelNodeTestCase(TestCase):
 
     @flush_nodes()
     def test_create_model_node(self):
@@ -117,30 +122,35 @@ class ModelNodeTestCase(ChemtrailsTestCase):
     def test_sync_related_branch(self):
         queryset = Store.objects.filter(pk__in=map(lambda n: n.pk,
                                                    StoreFixture(Store).create(count=2, commit=True)))
-        # store_nodeset = get_nodeset_for_queryset(queryset, sync=True, max_depth=0)
-        # for store in store_nodeset:
-        #     store_obj = store.get_object()
-        #
-        #     if store_obj.bestseller:
-        #         self.assertEqual(store.bestseller.get(), get_node_for_object(store_obj.bestseller))
-        #
-        #     self.assertEqual(len(store.books.all()), store_obj.books.count())
-        #     for book in store.books.all():
-        #         book_obj = book.get_object()
-        #         self.assertTrue(store in book.store_set.all())
-        #         self.assertEqual(book.publisher.get(), get_node_for_object(book_obj.publisher))
-        #         self.assertEqual(len(book.store_set.all()), book_obj.store_set.count())
-        #         self.assertEqual(len(book.bestseller_stores.all()), book_obj.bestseller_stores.count())
-        #
-        #         self.assertEqual(len(book.authors.all()), book_obj.authors.count())
-        #         # for author in book.authors.all():
-        #         #     author_obj = author.get_object()
-        #         #     l = author.book_set.all()
-        #         #     self.assertTrue(book in author.book_set.all())
-        #         #     self.assertEqual(author.user.get(), author_obj.user)
+        store_nodeset = get_nodeset_for_queryset(queryset, sync=True, max_depth=1)
+        for store in store_nodeset:
+            store_obj = store.get_object()
+
+            if store_obj.bestseller:
+                self.assertEqual(store.bestseller.get(), get_node_for_object(store_obj.bestseller))
+
+            self.assertEqual(len(store.books.all()), store_obj.books.count())
+            for book in store.books.all():
+                book_obj = book.get_object()
+                self.assertTrue(store in book.store_set.all())
+                self.assertEqual(book.publisher.get(), get_node_for_object(book_obj.publisher))
+                self.assertEqual(len(book.store_set.all()), book_obj.store_set.count())
+                self.assertEqual(len(book.bestseller_stores.all()), book_obj.bestseller_stores.count())
+                self.assertEqual(len(book.authors.all()), book_obj.authors.count())
+
+                # We have currently some issues with recursive max_depth. See #7
+                # FIXME: workaround - Sync nodeset for related queryset.
+                get_nodeset_for_queryset(book_obj.authors.all(), sync=True, max_depth=1)
+                for author in book.authors.all():
+                    author_obj = author.get_object()
+                    self.assertTrue(book in author.book_set.all())
+
+                    user = author.user.get()
+                    self.assertEqual(user, get_node_for_object(author_obj.user).sync())
+                    self.assertEqual(author, user.author.get())
 
 
-class MetaNodeTestCase(ChemtrailsTestCase):
+class MetaNodeTestCase(TestCase):
 
     def test_create_meta_node(self):
 
@@ -197,6 +207,7 @@ class MetaNodeTestCase(ChemtrailsTestCase):
         except ImproperlyConfigured as e:
             self.assertEqual(str(e), '%s must implement a Meta class.' % 'MetaNode')
 
+    @flush_nodes()
     @override_settings(CHEMTRAILS={
         'CONNECT_META_NODES': True
     })
