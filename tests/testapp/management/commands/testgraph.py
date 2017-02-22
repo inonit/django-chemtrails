e043813ed5e348e878e0a42341d660501f7e3f23
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-02-22 09:47:02 +0100

    Added recursive depth optional argument to testgraph command

--- tests/testapp/management/commands/testgraph.py
+++ tests/testapp/management/commands/testgraph.py
@@ -13,14 +13,23 @@ class Command(BaseCommand):
     help = 'Creates a bookstore test graph'
 
     def add_arguments(self, parser):
-        parser.add_argument('count', type=int)
+        parser.add_argument('count', type=int, help='Number of separate graph trees to create.')
+
+        parser.add_argument(
+            '--recursive-depth', '-r',
+            dest='depth',
+            default=1,
+            type=int,
+            help='Maximum depth of recursive connections to be made.'
+        )
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.NOTICE('Hang tight, this might take a little while...'))
         count = options['count']
+        depth = options['depth']
         queryset = Store.objects.filter(pk__in=map(lambda n: n.pk,
                                                    StoreFixture(Store).create(count=count, commit=True)))
-        get_nodeset_for_queryset(queryset, sync=True, max_depth=3)
+        get_nodeset_for_queryset(queryset, sync=True, max_depth=depth)
         self.stdout.write(self.style.SUCCESS(ungettext(
             'Successfully created %(count)d bookstore graph.\nCheck it out in the Neo4j web console!',
             'Successfully created %(count)d bookstore graphs.\nCheck them out in the Neo4j web console!', count
