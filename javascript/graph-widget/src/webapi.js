57085e5298729aa083d6057f8b959656b7bb57d6
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-03-09 15:31:30 +0100

    Remember to return from function ;)

--- javascript/graph-widget/src/webapi.js
+++ javascript/graph-widget/src/webapi.js
@@ -4,7 +4,7 @@ import 'isomorphic-fetch'
  * Fetch a list of nodes and their relation types
  */
 export function fetchNodeList() {
-  fetch('//localhost:8000/admin/chemtrails_permissions/accessrule/nodelist/').then((response) => {
+  return fetch('//localhost:8000/admin/chemtrails_permissions/accessrule/nodelist/').then((response) => {
     if (response.status >= 400) {
       throw new Error('Bad response from the server')
     }
