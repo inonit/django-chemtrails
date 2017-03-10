cc79cbc7e09587596bec2bda5bb4ef954a2f6137
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-03-10 10:09:21 +0100

    Include credentials in API call

--- javascript/graph-widget/src/webapi.js
+++ javascript/graph-widget/src/webapi.js
@@ -4,7 +4,9 @@ import 'isomorphic-fetch'
  * Fetch a list of nodes and their relation types
  */
 export function fetchNodeList() {
-  return fetch('//localhost:8000/admin/chemtrails_permissions/accessrule/nodelist/').then((response) => {
+  return fetch('//localhost:8000/admin/chemtrails_permissions/accessrule/nodelist/', {
+    credentials: 'include'
+  }).then((response) => {
     if (response.status >= 400) {
       throw new Error('Bad response from the server')
     }
