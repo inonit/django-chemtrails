4afabca63c1aed2157977833d842a1576ee23c0e
Nåkkve Kierulf <naakkve@gmail.com>
2017-03-20 15:20:56 +0100

    add direction marker to paths

--- javascript/graph-widget/src/components/Graph.js
+++ javascript/graph-widget/src/components/Graph.js
@@ -76,7 +76,37 @@ class Graph extends Component {
         nodes: this.state.nodes
       });
     });
+    svg
+      .append('svg:defs')
+      .selectAll('marker')
+      .data(['end']) // Different link/path types can be defined here
+      .enter()
+      .append('svg:marker') // This section adds in the arrows
+      .attr('id', String)
+      .attr('viewBox', '0 -5 10 10')
+      .attr('refX', 30)
+      .attr('refY', -1.5)
+      .attr('markerWidth', 10)
+      .attr('markerHeight', 10)
+      .attr('orient', 'auto')
+      .append('svg:path')
+      .attr('d', 'M0,-5L10,0L0,5');
 
+    var path = svg
+      .append('g')
+      .selectAll('path')
+      .data(this.state.links)
+      .enter()
+      .append('path')
+      .attr('class', function(d) {
+        return 'link ' + d.type;
+      })
+      .attr('id', function(d, i) {
+        return 'linkId_' + i;
+      })
+      .attr('fill', 'none')
+      .attr('stroke', 'black')
+      .attr('marker-end', 'url(#end)');
     let node = svg
       .append('g')
       .attr('class', 'nodes')
@@ -143,21 +173,6 @@ class Graph extends Component {
       .text(function(d) {
         return d.type;
       });
-
-    var path = svg
-      .append('g')
-      .selectAll('path')
-      .data(this.state.links)
-      .enter()
-      .append('path')
-      .attr('class', function(d) {
-        return 'link ' + d.type;
-      })
-      .attr('id', function(d, i) {
-        return 'linkId_' + i;
-      })
-      .attr('fill', 'none')
-      .attr('stroke', 'black');
 
     this.force.nodes(this.state.nodes);
     this.force.force('link').links(this.state.links);
