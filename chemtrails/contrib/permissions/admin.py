be5f53ed70e697af5c849c843607f108355db190
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-04-06 15:02:24 +0200

    Moved MetaGraphView to views file

--- chemtrails/contrib/permissions/admin.py
+++ chemtrails/contrib/permissions/admin.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from django.apps import apps
 from django.conf.urls import url, include
 from django.contrib import admin
-from neomodel import db
 
 from rest_framework import routers
 from rest_framework.decorators import api_view
@@ -11,11 +9,8 @@ from rest_framework.response import Response
 
 from chemtrails.contrib.permissions.forms import AccessRuleForm
 from chemtrails.contrib.permissions.models import AccessRule
-from chemtrails.contrib.permissions.serializers import NodeSerializer
-from chemtrails.contrib.permissions.views import AccessRuleViewSet
-from chemtrails.neoutils import get_meta_node_class_for_model
+from chemtrails.contrib.permissions.views import AccessRuleViewSet, MetaGraphView
 from chemtrails.neoutils.query import get_node_relationship_types
-from chemtrails.utils import flatten
 
 
 @admin.register(AccessRule)
@@ -38,10 +33,10 @@ class AccessRuleAdmin(admin.ModelAdmin):
 
         router = routers.DefaultRouter()
         router.register(r'access-rules', AccessRuleViewSet)
+        router.register(r'meta-graph', MetaGraphView, base_name='metagraph')
 
         info = self.model._meta.app_label, self.model._meta.model_name
         urlpatterns = [
-            url(r'^neo4j/meta-graph/$', self.get_meta_graph_api_view, name='%s_%s_meta_graph' % info),
             url(r'^neo4j/nodelist/$', self.get_nodelist_api_view, name='%s_%s_nodelist' % info),
             url(r'^neo4j/', include(router.urls))
         ] + super(AccessRuleAdmin, self).get_urls()
@@ -49,29 +44,8 @@ class AccessRuleAdmin(admin.ModelAdmin):
 
     @staticmethod
     @api_view(http_method_names=['GET'])
-    def get_meta_graph_api_view(request):
-        """
-        Returns the Meta graph serialized as JSON.
-        """
-        result, _ = db.cypher_query('MATCH (n {type: "MetaNode"}) RETURN n')
-
-        response = []
-        for item in list(flatten(result)):
-            try:
-                if hasattr(item, 'properties') and all(value in item.properties for value in ('app_label', 'model_name')):
-                    model = apps.get_model(app_label=item.properties['app_label'],
-                                           model_name=item.properties['model_name'])
-                    klass = get_meta_node_class_for_model(model)
-                    serializer = NodeSerializer(instance=klass.inflate(item), many=False)
-                    response.append(serializer.data)
-            except LookupError:
-                # Might trigger if getting data originating from a different source.
-                continue
-        return Response(data=response, content_type='application/json')
-
-    @staticmethod
-    @api_view(http_method_names=['GET'])
     def get_nodelist_api_view(request):
+        # TODO: This should be removed!!
         params = {'type': 'MetaNode'}
         result = get_node_relationship_types(params)
         return Response(data=result)
--- chemtrails/contrib/permissions/views.py
+++ chemtrails/contrib/permissions/views.py
@@ -1,12 +1,52 @@
 # -*- coding: utf-8 -*-
 
-from rest_framework.viewsets import ModelViewSet
+from django.apps import apps
+
+from neomodel import db
+
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet, ModelViewSet
 
 from chemtrails.contrib.permissions.models import AccessRule
-from chemtrails.contrib.permissions.serializers import AccessRuleSerializer
+from chemtrails.contrib.permissions.serializers import AccessRuleSerializer, NodeSerializer
+from chemtrails.neoutils import get_meta_node_class_for_model
+from chemtrails.utils import flatten
 
 
 class AccessRuleViewSet(ModelViewSet):
-
+    """
+    API View for access rules.
+    """
+    # authentication_classes = ()
+    # permission_classes = ()
     queryset = AccessRule.objects.all()
     serializer_class = AccessRuleSerializer
+
+
+class MetaGraphView(ViewSet):
+    """
+    API View for getting the meta graph.
+    """
+    # authentication_classes = ()
+    # permission_classes = ()
+
+    def list(self, request, format=None):
+        """
+        Returns the meta graph serialized as JSON.
+        """
+        result, _ = db.cypher_query('MATCH (n {type: "MetaNode"}) RETURN n')
+
+        response = []
+        for item in list(flatten(result)):
+            try:
+                if hasattr(item, 'properties') and all(
+                                value in item.properties for value in ('app_label', 'model_name')):
+                    model = apps.get_model(app_label=item.properties['app_label'],
+                                           model_name=item.properties['model_name'])
+                    klass = get_meta_node_class_for_model(model)
+                    serializer = NodeSerializer(instance=klass.inflate(item), many=False)
+                    response.append(serializer.data)
+            except LookupError:
+                # Might trigger if getting data originating from a different source.
+                continue
+        return Response(data=response, content_type='application/json')
