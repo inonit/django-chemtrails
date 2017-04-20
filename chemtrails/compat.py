bdaf6d748d0f6eb81b872288505386b916b141fd
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-04-20 13:22:37 +0200

    Using `is_authenticated()` from rest_framework as it's a dependency for the `chemtrails.contrib.permissions` module

--- chemtrails/compat.py
+++ chemtrails/compat.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import django
-
 # Support Django REST Framework
 try:
     import rest_framework
 except ImportError:
     rest_framework = None
-
-
-# Django 1.10 compatibility
-def is_authenticated(user):
-    if django.VERSION < (1, 10):
-        return user.is_authenticated()
-    return user.is_authenticated
