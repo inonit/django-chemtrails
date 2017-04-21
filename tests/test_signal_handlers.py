ffa4bcf50efa1d90f5238da1ce6df12d8950cfee
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-04-21 14:48:13 +0200

    Disconnect and reconnect original signal handlers for test cases

--- tests/test_signal_handlers.py
+++ tests/test_signal_handlers.py
@@ -14,6 +14,7 @@ class PostSaveHandlerTestCase(TestCase):
 
     @flush_nodes()
     def test_create_new_object_is_synced(self):
+        post_save.disconnect(post_save_handler, dispatch_uid='chemtrails.signals.handlers.post_save_handler')
         post_save.connect(post_save_handler, dispatch_uid='post_save_handler.test')
         try:
             book = BookFixture(Book).create_one()
@@ -22,10 +23,12 @@ class PostSaveHandlerTestCase(TestCase):
             self.assertEqual(book.pk, klass.nodes.get(pk=book.pk).pk)
             self.assertEqual(1, len(klass.nodes.has(authors=True, publisher=True)))
         finally:
+            post_save.connect(post_save_handler, dispatch_uid='chemtrails.signals.handlers.post_save_handler')
             post_save.disconnect(post_save_handler, dispatch_uid='post_save_handler.test')
 
     @flush_nodes()
     def test_delete_object_is_deleted(self):
+        pre_delete.disconnect(pre_delete_handler, dispatch_uid='chemtrails.signals.handlers.pre_delete_handler')
         pre_delete.connect(pre_delete_handler, dispatch_uid='pre_delete_handler.test')
         try:
             book = BookFixture(Book).create_one()
@@ -38,18 +41,22 @@ class PostSaveHandlerTestCase(TestCase):
             except klass.DoesNotExist as e:
                 self.assertEqual(str(e), "{'pk': %d}" % pk)
         finally:
+            pre_delete.connect(pre_delete_handler, dispatch_uid='chemtrails.signals.handlers.pre_delete_handler')
             pre_delete.disconnect(pre_delete_handler, dispatch_uid='pre_delete_handler.test')
 
     @flush_nodes()
     def test_m2m_changed_post_add_handler(self):
+        m2m_changed.disconnect(m2m_changed_handler, dispatch_uid='chemtrails.signals.handlers.m2m_changed_handler')
         m2m_changed.connect(m2m_changed_handler, dispatch_uid='m2m_changed_handler.test')
         try:
             pass
         finally:
+            m2m_changed.connect(m2m_changed_handler, dispatch_uid='chemtrails.signals.handlers.m2m_changed_handler')
             m2m_changed.disconnect(m2m_changed_handler, dispatch_uid='m2m_changed_handler.test')
 
     @flush_nodes()
     def test_m2m_changed_pre_clear(self):
+        m2m_changed.disconnect(m2m_changed_handler, dispatch_uid='chemtrails.signals.handlers.m2m_changed_handler')
         m2m_changed.connect(m2m_changed_handler, dispatch_uid='m2m_changed_handler.test')
         try:
             book = BookFixture(Book).create_one()
@@ -59,4 +66,5 @@ class PostSaveHandlerTestCase(TestCase):
             self.assertEqual(0, len(get_node_class_for_model(Book).nodes.has(authors=True)))
             self.assertEqual(0, len(get_node_class_for_model(Author).nodes.has(book_set=True)))
         finally:
+            m2m_changed.connect(m2m_changed_handler, dispatch_uid='chemtrails.signals.handlers.m2m_changed_handler')
             m2m_changed.disconnect(m2m_changed_handler, dispatch_uid='m2m_changed_handler.test')
