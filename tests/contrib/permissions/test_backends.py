f76d85eedbd2da449c7e375197188e6bc9afac9f
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-04-26 10:36:25 +0200

    Added unit tests for ChemoPermissionsBackend

--- tests/contrib/permissions/test_backends.py
+++ tests/contrib/permissions/test_backends.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, AnonymousUser, Group
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase, modify_settings
 
+from chemtrails.contrib.permissions.backends import ChemoPermissionsBackend
 from chemtrails.contrib.permissions.models import AccessRule
 from chemtrails.neoutils import get_nodeset_for_queryset
 
-from tests.utils import flush_nodes
+from tests.utils import flush_nodes, clear_neo4j_model_nodes
 from tests.testapp.autofixtures import Author, AuthorFixture, Store, StoreFixture
+
+User = get_user_model()
 
 
 class ChemoPermissionsBackendTestCase(TestCase):
@@ -22,15 +25,143 @@ class ChemoPermissionsBackendTestCase(TestCase):
             AUTHENTICATION_BACKENDS={'append': self.backend}
         )
         self.patched_settings.enable()
+        clear_neo4j_model_nodes()
 
     def tearDown(self):
         super(ChemoPermissionsBackendTestCase, self).tearDown()
         self.patched_settings.disable()
+        clear_neo4j_model_nodes()
 
-    def test_get_all_permissions(self):
-        pass
+    def test_authenticate(self):
+        kwargs = {
+            'username': 'testuser',
+            'password': 'test123.'
+        }
+        User.objects.create_user(**kwargs)
 
-    @flush_nodes()
+        backend = ChemoPermissionsBackend()
+        self.assertIsNone(backend.authenticate(**kwargs))
+
+    def test_get_all_permissions_user(self):
+        permissions = Permission.objects.filter(content_type__app_label='auth',
+                                                codename__in=[
+                                                    'add_user', 'change_user',
+                                                    'add_group', 'change_group'
+                                                ])
+        user = User.objects.create_user(username='testuser', password='test123.')
+        group = Group.objects.create(name='group')
+        user.user_permissions.add(*permissions)
+
+        backend = ChemoPermissionsBackend()
+
+        self.assertEqual(
+            set(permissions.filter(content_type__model='group').values_list('codename', flat=True)),
+            set(backend.get_all_permissions(user, group))
+        )
+        self.assertEqual(
+            set(permissions.filter(content_type__model='user').values_list('codename', flat=True)),
+            set(backend.get_all_permissions(user, user))
+        )
+
+    def test_get_all_permissions_group(self):
+        permissions = Permission.objects.filter(content_type__app_label='auth',
+                                                codename__in=[
+                                                    'add_user', 'change_user',
+                                                    'add_group', 'change_group'
+                                                ])
+        user = User.objects.create_user(username='testuser', password='test123.')
+        group = Group.objects.create(name='group')
+        group.user_set.add(user)
+        group.permissions.add(*permissions)
+
+        backend = ChemoPermissionsBackend()
+
+        self.assertEqual(
+            set(permissions.filter(content_type__model='group').values_list('codename', flat=True)),
+            set(backend.get_all_permissions(user, group))
+        )
+        self.assertEqual(
+            set(permissions.filter(content_type__model='user').values_list('codename', flat=True)),
+            set(backend.get_all_permissions(user, user))
+        )
+
+    def test_get_all_permissions_mixed_user_and_group(self):
+        permissions = Permission.objects.filter(content_type__app_label='auth',
+                                                codename__in=[
+                                                    'add_user', 'change_user',
+                                                    'add_group', 'change_group'
+                                                ])
+        user = User.objects.create_user(username='testuser', password='test123.')
+        group = Group.objects.create(name='group')
+        group.user_set.add(user)
+
+        user.user_permissions.add(*permissions.filter(content_type__model='user'))
+        group.permissions.add(*permissions.filter(content_type__model='group'))
+
+        backend = ChemoPermissionsBackend()
+
+        self.assertEqual(
+            set(permissions.filter(content_type__model='group').values_list('codename', flat=True)),
+            set(backend.get_all_permissions(user, group))
+        )
+        self.assertEqual(
+            set(permissions.filter(content_type__model='user').values_list('codename', flat=True)),
+            set(backend.get_all_permissions(user, user))
+        )
+
+    def test_get_all_permissions_non_model_instance(self):
+        class FooBar:
+            pass
+
+        obj = FooBar()
+        user = User.objects.create_user(username='testuser', password='test123.')
+        backend = ChemoPermissionsBackend()
+        self.assertEqual(set(), backend.get_all_permissions(user, obj))
+
+    def test_get_all_permissions_anonymous_user(self):
+        class FooBar:
+            pass
+
+        obj = FooBar()
+        user = AnonymousUser()
+        backend = ChemoPermissionsBackend()
+        self.assertEqual(set(), backend.get_all_permissions(user, obj))
+
+    def test_has_perm_non_model_instance(self):
+        class FooBar:
+            pass
+
+        obj = FooBar()
+        user = User.objects.create_user(username='testuser', password='test123.')
+        backend = ChemoPermissionsBackend()
+        self.assertFalse(backend.has_perm(user, 'app_label.codename', obj))
+
+    def test_has_perm_anonymous_user(self):
+        user = AnonymousUser()
+        group = Group.objects.create(name='group')
+
+        backend = ChemoPermissionsBackend()
+        self.assertFalse(backend.has_perm(user, 'app_label.codename', group))
+
+    def test_check_permission(self):
+        user = User.objects.create_user(username='testuser', password='test123.')
+
+        group = Group.objects.create(name='group')
+        group.user_set.add(user)
+
+        access_rule = AccessRule.objects.create(
+            ctype_source=ContentType.objects.get_for_model(User),
+            ctype_target=ContentType.objects.get_for_model(Group),
+            relation_types=['GROUPS']
+        )
+        perm = Permission.objects.get(content_type__app_label='auth', codename='add_group')
+        group.permissions.add(perm)
+        access_rule.permissions.add(perm)
+
+        backend = ChemoPermissionsBackend()
+        self.assertTrue(backend.has_perm(user, 'add_group', group))       # Permission codename only
+        self.assertTrue(backend.has_perm(user, 'auth.add_group', group))  # Full permission string
+
     def test_user_has_perm_single_relation_type(self):
         author = AuthorFixture(Author).create_one()
         permission = Permission.objects.get(codename='change_author')
@@ -43,7 +174,6 @@ class ChemoPermissionsBackendTestCase(TestCase):
         access_rule.permissions.add(permission)
         self.assertTrue(author.user.has_perm('testapp.change_author', author))
 
-    @flush_nodes()
     def test_user_has_perm_multiple_relation_types(self):
         # Set up a bookstore graph
         store = StoreFixture(Store).create_one()
@@ -65,5 +195,3 @@ class ChemoPermissionsBackendTestCase(TestCase):
         self.assertTrue(user.has_perm('testapp.add_store', store))
         self.assertTrue(user.has_perm('testapp.change_store', store))
         self.assertFalse(user.has_perm('testapp.delete_store', store))
-
-
