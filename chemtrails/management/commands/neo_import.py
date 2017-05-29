98578436fe377c3a25af679c688344de0b140b59
Nåkkve Kierulf <naakkve@gmail.com>
2017-05-29 09:27:31 +0200

    neo_import extended logging

--- chemtrails/management/commands/neo_import.py
+++ chemtrails/management/commands/neo_import.py
@@ -22,47 +22,58 @@ class Command(BaseCommand):
             for app in apps.all_models:
                 self.stdout.write(self.style.SUCCESS('Looking at {}'.format(app)))
                 cntr =0
-                for model in apps.get_app_config(app_label=app).get_models():
+                model_count =0
+
+                for n, model in enumerate(apps.get_app_config(app_label=app).get_models()):
+                    model_count += 1
                     cls = get_node_class_for_model(model)
                     if cls._is_ignored:
                         continue
 
                     for item in model.objects.all():
                         node = cls(instance=item, bind=False)
-
                         node.to_csv(cntr=cntr, target_file=target_file)
                         cntr += 1
 
             with open(target_file.name, newline='') as csvfile:
-
+                cypher = ''
                 spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
                 self.stdout.write(self.style.SUCCESS('Crucifixion party! Wait for it...'))
+                node_count = 0
                 skip_count = 0
-                for row in spamreader:
+                for n, row in enumerate(spamreader):
 
                     if row[0] == 'n':
                         # print(row[1])
                         try:
-                            db.cypher_query(row[1])
+                            cypher += row[1]
+                            node_count += 1
                         except exception.UniqueProperty:
                              skip_count+=1
-
+                db.cypher_query(cypher)
+                self.stdout.write(self.style.SUCCESS(
+                    '{} Nodes processed and delivered to Neo... '
+                    'Why oh why, didn\'t I take the blue pill??'.format(node_count)))
                 if skip_count > 0:
                     self.stdout.write(self.style.SUCCESS(
-                        '{} Nodes already existed, and was not updated'.format(skip_count)))
-
-                self.stdout.write(self.style.SUCCESS('Crucifixion party! By the left! Forward!'))
+                        '{} Nodes already existed, and was not updated.... Were you listening to me Neo? Or were you '
+                        'looking at the woman in the red dress?'.format(skip_count)))
 
             with open(target_file.name, newline='') as csvfile:
                 spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-                self.stdout.write(self.style.SUCCESS('Crucifixion party! Wait for it...'))
+                node_count = 0
                 for row in spamreader:
                     if row[0] == 'r':
                         # print(row[1])
                         db.cypher_query(row[1])
+                        node_count += 1
+
+                self.stdout.write(self.style.SUCCESS(
+                    '{} Relations processed and delivered to Neo... '
+                    'Why oh why, didn\'t I take the blue pill??'.format(node_count)))
                 self.stdout.write(self.style.SUCCESS('Crucifixion party! By the left! Forward!'))
 
-            self.stdout.write(self.style.SUCCESS('Successfully imported spam'))
+
         finally:
             target_file.close()
 
