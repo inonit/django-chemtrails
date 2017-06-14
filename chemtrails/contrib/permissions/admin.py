3720be44fc88edd8b29f28896e1c00f102ed57fd
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-06-14 22:45:46 +0200

    Added `toggle_active` admin action for access rules

--- chemtrails/contrib/permissions/admin.py
+++ chemtrails/contrib/permissions/admin.py
@@ -4,6 +4,8 @@ from django import forms
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.postgres.fields import ArrayField
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import routers
 from rest_framework.decorators import api_view
@@ -16,7 +18,7 @@ from chemtrails.neoutils.query import get_node_relationship_types
 
 @admin.register(AccessRule)
 class AccessRuleAdmin(admin.ModelAdmin):
-
+    actions = ('toggle_active',)
     list_display = ('ctype_target', 'ctype_source', 'requires_staff', 'is_active', 'created')
     list_filter = ('requires_staff', 'is_active', 'ctype_target')
     filter_horizontal = ('permissions',)
@@ -28,6 +30,19 @@ class AccessRuleAdmin(admin.ModelAdmin):
     formfield_overrides = {
         ArrayField: {'widget': forms.Textarea}
     }
+
+    def toggle_active(self, request, queryset):
+        """
+        Inverts the ``is_active`` flag of chosen access rules.
+        """
+        queryset.update(
+            is_active=models.Case(
+                models.When(is_active=True, then=models.Value(False)),
+                default=models.Value(True)))
+        self.message_user(request, _('Activated {0} and deactivated {1} '
+                                     'access rules.'.format(queryset.filter(is_active=True).count(),
+                                                            queryset.filter(is_active=False).count())))
+    toggle_active.short_description = _('Toggle active or inactive access rules')
 
     def get_urls(self):
 
@@ -49,4 +64,3 @@ class AccessRuleAdmin(admin.ModelAdmin):
         params = {'type': 'MetaNode'}
         result = get_node_relationship_types(params)
         return Response(data=result)
-
