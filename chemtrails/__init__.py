af50bb3d7f75094b7f60f12332780e6a39ccda8e
Rolf Håvard Blindheim <rhblind@gmail.com>
2017-12-04 11:50:51 +0100

    #49  - Bumped version
    Signed-off-by: Rolf Håvard Blindheim <rhblind@gmail.com>

--- chemtrails/__init__.py
+++ chemtrails/__init__.py
@@ -3,12 +3,8 @@
 default_app_config = 'chemtrails.apps.ChemtrailsConfig'
 
 __title__ = 'django-chemtrails'
-__version__ = '0.0.30'
+__version__ = '0.0.31'
 __author__ = 'Rolf Haavard Blindheim'
 __license__ = 'MIT License'
 
 VERSION = __version__
-
-__all__ = [
-    'settings'
-]
